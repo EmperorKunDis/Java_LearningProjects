1658decc6df635de9a4272fb578e99cd89ed2a9a
Martin Švanda <120254528+EmperorKunDis@users.noreply.github.com>
2023-09-10 08:06:34 +0200

    Elden Example

---
+++ Fibonacci/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BaseOfMath baseOfMath = new BaseOfMath();
+        baseOfMath.getClass();
+        System.out.println();
+
+    }
+}
