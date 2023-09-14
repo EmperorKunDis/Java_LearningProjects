9f80bffed13a666fc716666587f647430d16f0bd
Martin Švanda <120254528+EmperorKunDis@users.noreply.github.com>
2023-09-14 22:27:03 +0200

    Finally Complete

--- Fibonacci/src/BaseOfMath.java
+++ Fibonacci/src/BaseOfMath.java
@@ -1,13 +1,19 @@
+import java.util.HashMap;
 public class BaseOfMath {
     public static int fib(int n) {
+       return fib(n, new HashMap<>());
+    }
+    public static int fib(int n, HashMap<Integer, Integer> memo) {
         if (n == 0 || n == 1) {
             return n;
         }
-        return fib(n - 1) + fib(n - 2);
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
+        int result = fib(n - 1, memo) + fib(n - 2, memo);
+        memo.put(n, result);
+        return result;
     }
-
     public static void run() {
-
-
     }
 }
--- Fibonacci/src/Main.java
+++ Fibonacci/src/Main.java
@@ -1,10 +1,17 @@
+import javax.swing.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        BaseOfMath baseOfMath = new BaseOfMath();
-        baseOfMath.getClass();
-        System.out.println();
-
+       /* JFrame frame = new JFrame();
+        frame.setLayout();
+*/
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write the value of what number in the fibonacci sequence you want to know. :  ");
+            int n = scanner.nextInt();
+            int result = BaseOfMath.fib(n);
+            System.out.println("Value of " + n + "th number in the fibonacci sequence is : " + result);
 
     }
 }
